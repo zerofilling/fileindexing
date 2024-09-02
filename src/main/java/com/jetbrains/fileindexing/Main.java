caad9c37c75639ec0449a13f91e71d73f2713ebb
David Mkrtchyan <david.mkrtchyan@gmail.com>
2024-09-02 11:47:13 +0400

    Fix lightweight beans

--- src/main/java/com/jetbrains/fileindexing/Main.java
+++ src/main/java/com/jetbrains/fileindexing/Main.java
@@ -4,21 +4,30 @@ import com.google.common.collect.Lists;
 import com.jetbrains.fileindexing.config.Config;
 import com.jetbrains.fileindexing.processor.FileSearch;
 import com.jetbrains.fileindexing.search.TextContainsSearchStrategy;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
 import java.util.List;
 
+@Slf4j
 public class Main {
     public static void main(String[] args) {
         FileSearch fileSearch = FileSearch.builder().config(
                         Config.builder()
                                 .searchStrategy(new TextContainsSearchStrategy(
-//                                        new File("/Users/david/dev/fileindexing/.searchdata")))
                                         new File("/Users/david/dev/fileindexing/.searchdata")))
                                 .watchingFolders(Lists.newArrayList(new File("/Users/david/dev/fileindexing/src/test/resources")))
                                 .build())
                 .build();
-        List<File> result = fileSearch.search("fsd");
-        System.out.printf("");
+        while(true) {
+            try {
+                List<File> result = fileSearch.search("fsd");
+                log.info(result.toString());
+                break;
+            } catch (Exception e) {
+                log.info("search not ready");
+            }
+        }
+
     }
 }
